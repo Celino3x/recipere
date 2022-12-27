77ed1b4a0e2d41da43cb09285f0f3c64750bea80
Juscelino Campos <celino3x@gmail.com>
2022-12-27 14:53:56 -0300

    Views : Editar Visitantes, Footer, header, navbar e principal. Web, Welcome.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,63 +6,69 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Visitantes</title>
+    <title>RECIPERE</title>
 </head>
 <body>
     <header>
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">RECIPERE</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                  </li>
-                </ul>
+                <a class="navbar-brand" href="#">RECIPERE</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
 
-              </div>
+                </div>
             </div>
-          </nav>
+        </nav>
     </header>
 
     <section class="container">
-
         <form action="/cadastrar_visitante" method="POST">
+            @csrf
             <div class="mb-3">
               <label for="" class="form-label">Nome</label>
-              <input type="text" class="form-control" id="nome" >
+              <input type="text" class="form-control" id="nome" name="nome">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">CPF</label>
+                <input type="text" class="form-control" id="cpf" name="cpf" >
             </div>
 
             <div class="mb-3">
                 <label for="" class="form-label">Data Nascimento</label>
-                <input type="text" class="form-control" id="data_nascimento" >
+                <input type="date" class="form-control" id="data_nascimento" name="data_nascimento" >
             </div>
 
             <div class="mb-3">
                 <label for="" class="form-label">Endereço</label>
-                <input type="text" class="form-control" id="endereco" >
+                <input type="text" class="form-control" id="endereco" name="endereco">
             </div>
 
             <div class="mb-3">
                 <label for="" class="form-label">CEP</label>
-                <input type="text" class="form-control" id="cep" >
+                <input type="text" class="form-control" id="cep" name="cep">
             </div>
 
 
 
-            <button type="submit" class="btn btn-dark">Submit</button>
+            <button type="submit" class="btn btn-dark">Salvar</button>
           </form>
     </section>
     <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Visitante;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,43 @@ Route::get('/', function() {
 });
 
 Route::post('/cadastrar_visitante', function(Request $informacoes) {
-    dd($informacoes->all());
+    Visitante::create([
+        'nome' => $informacoes->nome,
+        'cpf' => $informacoes->cpf,
+        'data_nascimento' => $informacoes->data_nascimento,
+        'endereco' => $informacoes->endereco,
+        'cep' => $informacoes->cep
+    ]);
+    ?><section class="container">
+    <h1><?php echo "Visitante Cadastrado com Sucesso!"; ?></h1>
+    </section><?php
+});
+
+Route::get('/listar_visitantes/{id}', function($id) {
+    $visitante = Visitante::findOrFail($id);
+    echo $visitante->nome;
+    echo "<br/>";
+    echo $visitante->cpf;
+});
+
+Route::get('/editar_visitantes/{id}', function($id) {
+    $visitante = Visitante::findOrFail($id);
+    return view('editar_visitantes', ['visitante'=> $visitante]);
+});
+
+Route::put('/atualizar_visitante/{id}', function(Request $informacoes, $id){
+    $visitante = Visitante::findOrFail($id);
+    $visitante->nome = $informacoes->nome;
+    $visitante->cpf = $informacoes->cpf;
+    $visitante->data_nascimento = $informacoes->data_nascimento;
+    $visitante->endereco = $informacoes->endereco;
+    $visitante->cep = $informacoes->cep;
+    $visitante->save();
+    echo "Visitante Atualizado com Sucesso!";
+});
+
+Route::get('/excluir_visitante/{id}', function ($id){
+    $visitante = Visitante::findOrFail($id);
+    $visitante->delete();
+    echo "Visitante Ecluído com sucesso!";
 });
